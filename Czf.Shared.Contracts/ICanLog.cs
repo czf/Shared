24705a5344fc1636614a5c2c85a163b5381c3277
czf <fingarc@gmail.com>
2020-05-26 22:24:12 -0700

    Add project files.

---
+++ Czf.Shared.Contracts/ICanLog.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Czf.Shared.Contracts
+{
+    public interface ICanLog
+    {
+        void Error(string message);
+        void Error(Exception exception, string message = "");
+        void Info(string message);
+        void Warn(string message);
+        void Debug(string message);
+    }
+}
